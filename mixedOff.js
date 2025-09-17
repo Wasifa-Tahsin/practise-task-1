dbafe502875446f56841f49e4d3de9460c9a5175
Afia <arminakterafia@gmail.com>
2025-09-17 17:20:38 +0600

    add fruits

--- mixedOff.js
+++ mixedOff.js
@@ -37,7 +37,7 @@ var passed=true
 // 3.equal:==
 // 4.greater than or equal: >=
 // 5.less than or equal: <=
-// 6.not wqual: !=
+// 6.not equal: !=
 // -------
 
 // 7.And: &&
---
+++ string.js
@@ -0,0 +1,37 @@
+const country='Bangladesh'
+const division="NewKhale"
+const district=`B-Baria`
+const thana=new String('Demra')
+
+console.log(typeof division);
+console.log( thana);
+
+
+const numbers=[54,98,65,54,32]
+console.log(numbers.length);
+console.log(numbers[1]);
+
+
+
+const capital='Dha ka'
+console.log(capital.length);
+
+
+
+const subject='chemistry'
+const book='chemistry'
+
+if (subject===book) {
+    console.log('I am reading book ');
+    
+}
+else{
+    console.log('hudai portesi');
+}
+
+
+
+
+const fruits = ['apple','banana','orange','lichi'];
+console.log(fruits.length); // 4
+
