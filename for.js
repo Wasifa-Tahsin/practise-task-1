4ad2c788950106aa4ce2cea076a388b1d5da7be4
Afia <arminakterafia@gmail.com>
2025-09-20 21:47:26 +0600

    practice

---
+++ for.js
@@ -0,0 +1,28 @@
+let num=1
+while(num<=10){
+    console.log(num);
+    num++
+}
+
+
+for(let num=0;num<=10;num++){
+    console.log(num);
+}
+
+let array=[23,343,56,67,87,4,3,354]
+// for loop structure
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+
+// ---odd
+for(let i=1;i<=20;i=i+2){
+    console.log(i);
+}
+
+//---- even
+for(let i=0;i<=20;i=i+2){
+    console.log(i);
+}
