6d8d2f91923d8d11a0ec4cadc7c3bf49d3c4e0b5
Afia <arminakterafia@gmail.com>
2025-09-20 18:52:55 +0600

    practice

--- loop.js
+++ loop.js
@@ -6,11 +6,11 @@
 
 // }
 // array
-const numbers=[23,35,57,78,89,5,4,23,223]
-console.log(numbers);
-for(const num of numbers){
-    console.log(num);
-}
+// const numbers=[23,35,57,78,89,5,4,23,223]
+// console.log(numbers);
+// for(const num of numbers){
+//     console.log(num);
+// }
 
 
 
@@ -26,9 +26,47 @@ for(const num of numbers){
 // }
 
 
-// while loop
-let num=0
-while (num<5) {
-    console.log('looping',num);
-    num=num+1
+//----------------- while loop
+// let num=0
+// while (num<5) {
+//     console.log('looping',num);
+//     num=num+1
+// }
+
+
+// let num=1
+// while (num<=10) {
+//     console.log(num);
+//     num=num+1
+// }
+
+// let num=1
+// while(num<=100){
+//     console.log(num);
+//     num++
+// }
+
+
+// jor songkha
+// let num=1
+// let sum=0
+// while (num<=10) {
+//     console.log(num);
+//     sum=sum+num
+//     console.log('sum',sum);
+//     num++
+// }
+
+
+
+
+// bijor
+let num=1
+while (num<=10) {
+    console.log(num);
+    num++
+    if (num%2===0) {
+        console.log('even number',num);
+    }
+    
 }
